ac4ff56bebadf3cacc6fe239d0df167d3aa88371
kingsj <seajin2003@gmail.com>
2019-06-18 10:14:01 +0900

    추가 ㅏㅎㄴ줄 추가

--- test1/src/test1/Hello.java
+++ test1/src/test1/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("He's name is Tom.");
 		System.out.println("Her name is Janee");
 		System.out.println("My name is sejin");
+		System.out.println("Ãß°¡");
 
 	}
 
